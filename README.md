635c7ca12557061c9eda1340dd82253f865265d3
Raghab Shrestha <gunnerraghab@gmail.com>
2023-10-19 16:21:15 +0545

    instrunction added to run the file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Kutuko
 NodeJs coding challenge
+To RUN the code
+install node
+intall npm
+npm install
+npm start
+
+
--- src/Mapper/RssMapper.ts
+++
@@ -1,8 +0,0 @@
-export class ProductEntryMapper{
-    async ModelToDto(model){
-        let dto = []
-        for(let mod of model){
-            
-        }
-    }
-}
